a35cd361c46b63548ab316718246cf572a84e36a
Nick Howlett <agiel882@gmail.com>
2019-03-09 16:48:38 -0800

    Create redux loop, route utils and app component

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Flex Project</h1>
+            </div>
+        )
+    }
+}
---
+++ frontend/src/util/route_util.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect, withRouter } from 'react-router-dom';
+
+const Auth = ({ component: Component, path, loggedIn, exact}) => (
+    <Route path={path} exact={exact} render={(props) => (
+        !loggedIn ? (
+            <Component {...props} />
+        ) : (
+            <Redirect to="/profile" />
+        )
+    )} />
+);
+
+const Protected = ({ component: Component, loggedIn, ...rest }) => (
+    <Route 
+        {...rest}
+        rende={props => 
+            loggedIn ? (
+                <Component {...props} />
+            ) : (
+                <Redirect to="/" />
+            )
+        }
+    />
+);
+
+const msp = state => (
+    {loggedIn: state.session.isAuthenticated}
+);
+
+export const AuthRoute = withRouter(connect(msp)(Auth));
+
+export const ProtectedRoute = withRouter(connect(msp)(Protected));
