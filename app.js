5da03bbf781894e07c50fe0505ddd7135b9822fa
unknown <agiel882@gmail.com>
2019-03-11 16:05:00 -0700

    Fix client push

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
     app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
 }
 
