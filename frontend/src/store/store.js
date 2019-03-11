3fd4ca52808856ce0086a7868defe7a771f3f1b1
unknown <agiel882@gmail.com>
2019-03-11 16:25:07 -0700

    Add logger to heroku deploy

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -5,7 +5,7 @@ import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
-    let middleware = [thunk];
+    let middleware = [thunk, logger];
     if (process.env.NODE_ENV !== 'production') {
       middleware = [...middleware, logger];
     }
