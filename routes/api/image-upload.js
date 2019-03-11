8f29e459f1192afd732433641d2bcad6b798eecb
DrewE <andrew.engelstein@gmail.com>
2019-03-11 15:16:39 -0700

    Update response from aws upload to return key

--- routes/api/image-upload.js
+++ routes/api/image-upload.js
@@ -7,7 +7,7 @@ const singleUpload = upload.single("image");
 
 router.post("/image-upload", (req, res) => {
   singleUpload(req, res, err => {
-    return res.json({ imageUrl: req.file.location });
+    return res.json({ imageUrl: req.file.key });
   });
 });
 
