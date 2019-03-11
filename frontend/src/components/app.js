d0cbac84676ef75a62fb5e723093c2e53f69e740
unknown <agiel882@gmail.com>
2019-03-11 11:47:19 -0700

    Implement basic profile styling

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,19 +3,19 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Route, Switch } from 'react-router-dom';
 import NavbarContainer from './nav/navbar_container';
 import Splash from './splash/splash';
-import Profile from './profile/profile';
+import ProfileContainer from './profile/profile_container';
 import "./App.scss";
 import Journey from './journey/Journey';
 
 const App = () => {
     return(
         <>  
-            <Route path="/journey" component={NavbarContainer}></Route>
+            <Route path="/journeys" component={NavbarContainer}></Route>
             <Route path="/profile" component={NavbarContainer}></Route>
             <Route path="/map" component={NavbarContainer}></Route>
             <Switch>
                 <AuthRoute exact path="/" component={Splash} />
-                <ProtectedRoute exact path="/profile" component={Profile} />
+                <ProtectedRoute exact path="/profile" component={ProfileContainer} />
             </Switch>
         </>
     )
