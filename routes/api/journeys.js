df13740adf016841dca959a3dbd717ae555e62f5
Mason Chinkin <mason.chinkin@gmail.com>
2019-03-10 19:24:43 -0700

    Removed old journey get route

--- routes/api/journeys.js
+++ routes/api/journeys.js
@@ -5,24 +5,6 @@ const Journey = require('../../models/Journey');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the journeys route" }));
 
-router.post('/',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    // const { isValid, errors } = validateJourneyInput(req.body);
-
-    // if (!isValid) return res.status(400).json.errors;
-
-    const newJourney = new Journey({
-      name: req.body.name,
-      description: req.body.description,
-      userId: req.user.id
-    });
-
-    newJourney
-      .save();
-  }
-);
-
 router.post('/journeys',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
