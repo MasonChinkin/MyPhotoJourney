63d3f539a4b083ccc11725b0bdd171797663ae76
unknown <agiel882@gmail.com>
2019-03-10 19:36:48 -0700

    Add journey errors actions

--- frontend/src/actions/journey_actions.js
+++ frontend/src/actions/journey_actions.js
@@ -1,10 +1,16 @@
 import * as JourneysUtils from '../util/journeys_util';
 
 export const RECEIVE_JOURNEY = "RECEIVE_JOURNEY";
+export const RECEIVE_JOURNEY_ERRORS = "RECEIVE_JOURNEY_ERRORS";
 
 export const receiveJourney = journeyPayLoad => ({
   type: RECEIVE_JOURNEY,
   journeyPayLoad
+});
+
+export const receiveErrors = errs => ({
+  type: RECEIVE_JOURNEY_ERRORS,
+  errs
 });
 
 export const requestJourney = id => dispatch => (
