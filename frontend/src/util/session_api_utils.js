afd6120f6280c0f0bf79ff068b029cbf16a34e66
unknown <agiel882@gmail.com>
2019-03-11 17:03:31 -0700

    Unfix urls to test

--- frontend/src/util/session_api_utils.js
+++ frontend/src/util/session_api_utils.js
@@ -9,14 +9,9 @@ export const setAuthToken = token => {
 };
 
 export const signup = (userData) => {
-  return axios.post('api/users/register', userData);
+  return axios.post('/api/users/register', userData);
 };
 
 export const login = (userData) => {
-  return axios.post('api/users/login', userData);
-  // return axios.request({
-  //   method: "POST",
-  //   url: "https://myphotojourney.herokuapp.com/api/users/login",
-  //   data: userData
-  // });
+  return axios.post('/api/users/login', userData);
 };
