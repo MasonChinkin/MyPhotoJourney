3d8ab88afb7624917a00bb4f69274b27c9bc2685
DrewE <andrew.engelstein@gmail.com>
2019-03-12 09:23:05 -0700

    Fix post journey response to include description

--- routes/api/journeys.js
+++ routes/api/journeys.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const Journey = require('../../models/Journey');
-const Photo = require('../../models/Photo');
-const validateJourneyInput = require('../../validation/journeys');
-const NodeGeocoder = require('node-geocoder');
-const geocoder = NodeGeocoder({provider: "openstreetmap"});
+const passport = require("passport");
+const Journey = require("../../models/Journey");
+const Photo = require("../../models/Photo");
+const validateJourneyInput = require("../../validation/journeys");
+const NodeGeocoder = require("node-geocoder");
+const geocoder = NodeGeocoder({ provider: "openstreetmap" });
 
-router.get("/test", (req, res) => res.json({ msg: "This is the journeys route" }));
+router.get("/test", (req, res) =>
+  res.json({ msg: "This is the journeys route" })
+);
 
-router.post('/',
+router.post(
+  "/",
   // passport.authenticate('jwt', { session: false }),
   async (req, res) => {
     debugger;
 
-    const { errors, isValid } = await validateJourneyInput(req.body)    
-    
+    const { errors, isValid } = await validateJourneyInput(req.body);
+
     if (!isValid) {
       return res.status(400).json(errors);
     }
@@ -25,31 +28,28 @@ router.post('/',
       userId: req.body.user.id
     });
 
-    if (req.body.description) {
+    if (req.body.journey.description) {
       newJourney.description = req.body.journey.description;
     }
 
     newJourney.save(function(err, newJourney) {
-      if(err) {
+      if (err) {
         return res.status(400).json(err);
       } else {
         debugger;
         return res.status(200).json(newJourney);
       }
-    }); 
+    });
   }
 );
 
-router.get('/:journey_id',
-  (req, res) => {
-    Promise.all([
-      Journey.findById(req.params.journey_id),
-      Photo
-        .find({ journeyId: req.params.journey_id })
-        .sort({ photoDateTime: 1 })
-    ]).then((journey, photos) => res.json({ journey, photos }))
-      .catch(err => res.status(400).json(err));
-  }
-);
+router.get("/:journey_id", (req, res) => {
+  Promise.all([
+    Journey.findById(req.params.journey_id),
+    Photo.find({ journeyId: req.params.journey_id }).sort({ photoDateTime: 1 })
+  ])
+    .then((journey, photos) => res.json({ journey, photos }))
+    .catch(err => res.status(400).json(err));
+});
 
 module.exports = router;
