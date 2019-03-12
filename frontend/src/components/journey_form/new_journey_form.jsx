6fd74f6b07693e4879f4cd1bcc3e1068a13b1112
DrewE <andrew.engelstein@gmail.com>
2019-03-11 22:05:43 -0700

    Add styling to journey form

--- frontend/src/components/journey_form/new_journey_form.jsx
+++ frontend/src/components/journey_form/new_journey_form.jsx
@@ -43,40 +43,51 @@ class NewJourneyForm extends React.Component {
 
   render() {
     return (
-      <div className="center flex column">
+      <div className="create-journey">
+        <div>
+          <h1>Create a Photo Journey</h1>
+        </div>
         <form id="new-journey-form" onSubmit={this.handleSubmit}>
-          <h2>Name</h2>
-          <input
-            id="journey-name"
-            type="text"
-            value={this.state.name}
-            onChange={this.handleChange("name")}
-          />
-          <h2>Description</h2>
-          <textarea
-            id="journey-description"
-            value={this.state.description}
-            onChange={this.handleChange("description")}
-          />
-          <input
-            type="file"
-            id="photo-upload"
-            style={{ display: "none" }}
-            multiple
-            accept="image/*"
-            onChange={this.handleFile}
-          />
-          <input
-            className="button create"
-            type="submit"
-            value="Upload Photos"
-            onClick={() => {
-              const upload = document.getElementById("photo-upload");
-              upload.click();
-            }}
-          />
+          <div className="form-fields">
+            <div>
+              <h2>Name</h2>
+              <input
+                id="journey-name"
+                type="text"
+                value={this.state.name}
+                onChange={this.handleChange("name")}
+              />
+            </div>
+            <div>
+              <h2>Description</h2>
+              <textarea
+                id="journey-description"
+                value={this.state.description}
+                onChange={this.handleChange("description")}
+              />
+            </div>
+          </div>
+          <div className="submit-button">
+            <input
+              type="file"
+              id="photo-upload"
+              style={{ display: "none" }}
+              multiple
+              accept="image/*"
+              onChange={this.handleFile}
+            />
+            <input
+              className="button create upload-photos"
+              type="submit"
+              value="Upload Photos"
+              onClick={() => {
+                const upload = document.getElementById("photo-upload");
+                upload.click();
+              }}
+            />
+          </div>
         </form>
-        <div className="photoPreview flex">
+        <div className="photoPreview">
           {this.state.files.map((file, idx) => {
             return <PhotoUploadForm key={idx} file={file} />;
           })}
