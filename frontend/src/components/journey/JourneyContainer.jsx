a50e12689ffbc01877cd3763ecc87bb4b044bddf
Mason Chinkin <mason.chinkin@gmail.com>
2019-03-12 21:51:48 -0700

    Clean code

--- frontend/src/components/journey/JourneyContainer.jsx
+++ frontend/src/components/journey/JourneyContainer.jsx
@@ -1,7 +1,5 @@
 import { connect } from 'react-redux';
-import JourneyMap from './JourneyMap';
 import { withRouter } from 'react-router-dom';
-import { requestJourney } from '../../actions/journey_actions';
 import Journey from './Journey';
 
 const mSP = ({ entities }, ownProps) => ({
