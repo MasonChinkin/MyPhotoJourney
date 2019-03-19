4036f0df6ae727ed7dff1ecd22bd5913ba3e42d0
DrewE <andrew.engelstein@gmail.com>
2019-03-19 16:57:17 -0700

    Refactor JourneyListItem in a class component, add copy URL functionality to the copy icon

--- frontend/src/components/profile/journey_list_item.jsx
+++ frontend/src/components/profile/journey_list_item.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default ({ journey }) => {
-  return (
-    <div className="journey-list-item">
-      <Link to={`/journey/${journey._id}`}>
-        <span>{journey.name}</span>
-      </Link>
-      <i className="fa fa-clipboard" />
-    </div>
-  )
+export default class JourneyListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.copyURL = this.copyURL.bind(this);
+  }
+
+  copyURL() {
+    let el = document.createElement("textarea");
+    document.body.appendChild(el);
+    el.value = `https://myphotojourney.herokuapp.com/#/journey/${
+      this.props.journey._id
+    }`;
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+    alert(`URL copied to clipboard!`);
+  }
+
+  render() {
+    return (
+      <div className="journey-list-item">
+        <Link to={`/journey/${this.props.journey._id}`}>
+          <span>{this.props.journey.name}</span>
+        </Link>
+        <i className="fa fa-clipboard" onClick={this.copyURL} />
+      </div>
+    );
+  }
 }
