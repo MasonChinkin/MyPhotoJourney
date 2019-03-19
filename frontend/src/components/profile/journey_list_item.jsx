790ea9c8ddce60a47257d4569f4d96416bb56eb7
unknown <agiel882@gmail.com>
2019-03-19 16:58:49 -0700

    Add removal functionality to profile page

--- frontend/src/components/profile/journey_list_item.jsx
+++ frontend/src/components/profile/journey_list_item.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
-export default ({ journey }) => {
-  return (
-    <div className="journey-list-item">
-      <Link to={`/journey/${journey._id}`}>
-        <span>{journey.name}</span>
-      </Link>
-      <i className="fa fa-clipboard" />
-    </div>
-  )
+class JourneyListItem extends React.Component{
+
+
+   deleteJourney(){
+    confirmAlert({
+       message: 'Are you sure to delete this journey?',
+      buttons: [
+      {
+        label: 'Yes',
+        onClick: () => this.props.deleteJourney(this.props.journey._id)
+        },
+      {
+        label: 'No',
+        onClick: () => {}
+        }
+      ]
+    });
+  }
+
+  render(){
+    const journey = this.props.journey;
+    return (
+      <div className="journey-list-item">
+        <Link to={`/journey/${journey._id}`}>
+          <span>{journey.name}</span>
+        </Link>
+        <i className="fa fa-clipboard"/>
+        <i className="fa fa-remove" style={{marginLeft: "5px", fontSize: "26px", color: "red"}} onClick={this.deleteJourney.bind(this)}/>
+      </div>
+    )
+  }
 }
+
+export default JourneyListItem
