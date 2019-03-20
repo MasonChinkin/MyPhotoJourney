16a8f571c50b81810ac3525adb6feaf7e0b27d38
DrewE <andrew.engelstein@gmail.com>
2019-03-19 17:26:23 -0700

    Add styling to linkedIn and Github buttons

--- frontend/src/components/about/abouts.js
+++ frontend/src/components/about/abouts.js
@@ -1,44 +1,60 @@
-import React from 'react';
+import React from "react";
 
 export default () => {
-    return (
-        <div id="abouts">
-            <div id="git-list" className="about-list">
-                <div>
-                    <h1>Nick Howlette</h1>
-                    <a href="https://github.com/Nick-Howlett"><img src="./GitHub-Mark-64px.png" alt="Github Logo" /></a>
-                </div>
-                <div>
-                    <h1>Mason Chinkin</h1>
-                    <a href="https://github.com/MasonChinkin"><img src="./GitHub-Mark-64px.png" alt="Github Logo" /></a>
-                </div>
-                <div>
-                    <h1>Louis Leon</h1>
-                    <a href="https://github.com/Louis-C-Leon"><img src="./GitHub-Mark-64px.png" alt="Github Logo" /></a>
-                </div>
-                <div>
-                    <h1>Drew Engelstein</h1>
-                    <a href="https://github.com/ase1210"><img src="./GitHub-Mark-64px.png" alt="Github Logo" /></a>
-                </div>
-            </div>
-            <div id="linkedin-list" className="about-list">
-                <div>
-                    <h1>Nick Howlette</h1>
-                    <a href="https://www.linkedin.com/in/nick-howlett/"><img src="./linkedin_logo.png" height="64" alt="LinkedIn Logo" /></a>
-                </div>
-                <div>
-                    <h1>Mason Chinkin</h1>
-                    <a href="https://www.linkedin.com/in/mason-chinkin/"><img src="./linkedin_logo.png" height="64" alt="LinkedIn Logo" /></a>
-                </div>
-                <div>
-                    <h1>Louis Leon</h1>
-                    <a href="https://www.linkedin.com/in/louis-leon-239bb2173/"><img src="./linkedin_logo.png" height="64" alt="LinkedIn Logo" /></a>
-                </div>
-                <div>
-                    <h1>Drew Engelstein</h1>
-                    <a href="https://www.linkedin.com/in/andrew-engelstein-01938911/"><img src="./linkedin_logo.png" height="64" alt="LinkedIn Logo" /></a>
-                </div>
-            </div>
+  return (
+    <div id="abouts">
+      <div id="git-list" className="about-list">
+        <div>
+          <a href="https://github.com/Nick-Howlett">
+            Nick Howlette
+            <img src="./GitHub-Mark-64px.png" alt="Github Logo" />
+          </a>
         </div>
-    )
-}
+        <div>
+          <a href="https://github.com/MasonChinkin">
+            Mason Chinkin
+            <img src="./GitHub-Mark-64px.png" alt="Github Logo" />
+          </a>
+        </div>
+        <div>
+          <a href="https://github.com/Louis-C-Leon">
+            Louis Leon
+            <img src="./GitHub-Mark-64px.png" alt="Github Logo" />
+          </a>
+        </div>
+        <div>
+          <a href="https://github.com/ase1210">
+            Drew Engelstein
+            <img src="./GitHub-Mark-64px.png" alt="Github Logo" />
+          </a>
+        </div>
+      </div>
+      <div id="linkedin-list" className="about-list">
+        <div>
+          <a href="https://www.linkedin.com/in/nick-howlett/">
+            Nick Howlette
+            <img src="./linkedin_logo.png" height="64" alt="LinkedIn Logo" />
+          </a>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/mason-chinkin/">
+            Mason Chinkin
+            <img src="./linkedin_logo.png" height="64" alt="LinkedIn Logo" />
+          </a>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/louis-leon-239bb2173/">
+            Louis Leon
+            <img src="./linkedin_logo.png" height="64" alt="LinkedIn Logo" />
+          </a>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/andrew-engelstein-01938911/">
+            Drew Engelstein
+            <img src="./linkedin_logo.png" height="64" alt="LinkedIn Logo" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
