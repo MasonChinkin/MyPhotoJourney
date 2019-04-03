09ccd912b05ad714c9222a51d64a8a4e97923cb0
Louis Leon <louis.leon@utexas.edu>
2019-04-03 15:57:41 -0700

    modify number of arrows on map

--- frontend/src/util/map_util.js
+++ frontend/src/util/map_util.js
@@ -9,7 +9,7 @@ export function drawMap(state, anchor) {
   const h = 491;
 
   let photos = state.data[1];
-  numArrows = (photos.length - 1) * 2
+  numArrows = photos.length > 1 ? photos.length : 0
 
   // Scale should range between 1.25 (min zoom of 1 where .8 * 1.25 = 1), with a max zoom of 10x
   let scale = Math.max(Math.min(getScale(photos), 10), 1.25);
