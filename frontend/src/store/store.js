7bedf4a1d7b24d39ca363c7cf35e12f07957dea0
Mason Chinkin <mason.chinkin@gmail.com>
2019-04-12 10:06:15 -0700

    Add reduc chrome dev tool to store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,19 +1,27 @@
-import { createStore, applyMiddleware } from 'redux';
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
-    let middleware = [thunk];
-    if (process.env.NODE_ENV !== 'production') {
-      middleware = [...middleware, logger];
-    }
-    return createStore(
-      rootReducer,
-      preloadedState,
+  let middleware = [thunk];
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  if (process.env.NODE_ENV !== 'production') {
+    middleware = [...middleware, logger];
+  }
+
+  return createStore(
+    rootReducer,
+    preloadedState,
+    composeEnhancers(
       applyMiddleware(...middleware)
-    );
-  };
+    ));
+};
 
 export default configureStore;
